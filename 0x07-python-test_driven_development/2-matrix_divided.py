23f4f60e33e6fcffd9b32f59ae54eea215c71c56
FARAHrafah123 <farahrahali9876@gmail.com>
2023-09-05 17:21:34 -0700

    add fils

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,38 +1,25 @@
 #!/usr/bin/python3
 """
-Module matrix_divided
+
+function that divides all elements of a matrix
+
 """
 
 
 def matrix_divided(matrix, div):
-    """Returns a new matrix (list of lists)
     """
-
-    if not isinstance(matrix, list) or len(matrix) == 0 or not matrix[0]:
-        raise TypeError("matrix must be a matrix (list of lists) " +
-                        "of integers/floats")
-
-    for row in matrix:
-        if len(row) == 0:
-            raise TypeError("matrix must be a matrix (list of lists) " +
-                            "of integers/floats")
-        for col in row:
-            if type(col) is not int and type(col) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) " +
-                                "of integers/floats")
-
-    len_rows = []
-    for row in matrix:
-        len_rows.append(len(row))
-    if not all(col == len_rows[0] for col in len_rows):
-        raise TypeError("Each row of the matrix must have the same size")
-
-    if type(div) is not int and type(div) is not float:
+    function that divides all elements of a matrix
+    """
+    sizeErr = "Each row of the matrix must have the same size"
+    typeErr = "matrix must be a matrix (list of lists) of integers/floats"
+    for x in matrix:
+        if len(x) != len(matrix[0]):
+            raise TypeError(sizeErr)
+        for y in x:
+            if not isinstance(y, (int, float)):
+                raise TypeError(typeErr)
+    if not (isinstance(div, (int, float))):
         raise TypeError("div must be a number")
-
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
-    new_matrix = [[round(col / div, 2) for col in row] for row in matrix]
-
-    return new_matrix
+    return [[round(y / div, 2) for y in x] for x in matrix]
