a4140bd07fd13c7068c169a64112e1c209770835
FARAHrafah123 <farahrahali9876@gmail.com>
2023-10-17 11:54:46 -0700

    add files

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,2 @@
---The script that computes the score average of all records in the table second_table of the database hbtn_0c_0
-SELECT AVG(score) average FROM second_table;
-
+-- average
+SELECT AVG(score) AS average FROM second_table;
